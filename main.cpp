a8d3d1317f528f182fc19f5897da4bbf43a6dd74
hardw3q <sob.dod@gmail.com>
2024-11-06 13:18:37 +0300

    Egorov stack class

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-
+#define size 25
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
 class Stack{
-private:
-    int lop;
-
+    private:
+        int top;
+        T mem[size]
+    public:
+        Stack(){
+            top = -1
+        }
+        void push(T val){
+            top++;
+            mem[top] = val;
+        }
+        T pop(){
+            T result = mem[top];
+            top--
+            return result;
+        }
 };
