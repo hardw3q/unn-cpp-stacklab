64090813f4ba07841244235d8cea26cbea79a971
hardw3q <sob.dod@gmail.com>
2024-11-06 13:25:11 +0300

    Egorov vector stack

--- main.cpp
+++ main.cpp
@@ -4,21 +4,41 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+// // Первая реализация с помощью массива
+//class Stack{
+//    private:
+//        int top;
+//        T mem[size]
+//    public:
+//        Stack(){
+//            top = -1
+//        }
+//        void push(T val){
+//            top++;
+//            mem[top] = val;
+//        }
+//        T pop(){
+//            T result = mem[top];
+//            top--
+//            return result;
+//        }
+//};
+
+// std:vector
+// push_back()
+// pop_back()
+
 class Stack{
     private:
         int top;
-        T mem[size]
+        std::vector<T> mem;
     public:
-        Stack(){
-            top = -1
-        }
         void push(T val){
             top++;
-            mem[top] = val;
+            mem.push_back(val);
         }
-        T pop(){
-            T result = mem[top];
-            top--
-            return result;
+        void pop(){
+            return mem.pop_back();
         }
+
 };
